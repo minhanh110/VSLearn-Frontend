0e59c4459a9aab6b2ac2e14a2c1f9ad2c3fc5a1e
dhquan2110jypiece <dhquan2110@gmail.com>
2025-08-08 20:32:23 +0700

    Revert "Update revenue-dashboard/page.tsx: cập nhật thống kê doanh thu"
    This reverts commit 1e2bbdfa1e3a6c02e00866ea82861380f10d92b6.

--- frontend-react/app/general-manager/revenue-dashboard/page.tsx
+++ frontend-react/app/general-manager/revenue-dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import RevenueDashboardComponent from "./revenue-dashboard-real"
+import RevenueDashboardComponent from "./revenue-dashboard-page"
 
 export default function Page() {
   return <RevenueDashboardComponent />
