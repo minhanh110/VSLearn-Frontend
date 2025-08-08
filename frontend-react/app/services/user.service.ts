3d1377fb7a1f7b7cd778533b581f3a8002f50370
Phuonglinh2623 <phuonglinh206203@gmail.com>
2025-08-08 18:08:24 +0700

    Update user.service.ts: cập nhật API người dùng

--- frontend-react/app/services/user.service.ts
+++ frontend-react/app/services/user.service.ts
@@ -1,8 +1,8 @@
-import axiosInstance from './axios.config';
+﻿import axiosInstance from './axios.config';
 import Cookies from 'js-cookie';
 import authService from './auth.service';
 
-const API_URL = 'http://localhost:8080/users';
+const API_URL = '/users';
 
 export interface ProfileData {
     firstName?: string;
